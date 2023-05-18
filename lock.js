96bcb622ad012338e1f53072205a641afaa0842d
Norbert Toth <118756875+dil-ntoth@users.noreply.github.com>
2023-05-18 14:06:57 +0200

    js test

---
+++ lock.js
@@ -0,0 +1,30 @@
+import { Octokit } from "octokit";
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: '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'
+});
+const { data: { login }, } = await octokit.rest.users.getAuthenticated();
+// console.log("Hello, %s", login);
+await octokit.request('PUT /repos/{owner}/{repo}/branches/{branch}/protection', {
+  owner: 'dil-ntoth',
+  repo: 'actions_test',
+  branch: 'integration',
+  "required_pull_request_reviews": {
+    "dismiss_stale_reviews": true,
+    "require_code_owner_reviews": true,
+    "required_approving_review_count": 1
+    },
+    "required_status_checks": {
+    "strict": false,
+    "contexts": []
+    },
+    "restrictions": null,
+    "required_signatures": false,
+    "enforce_admins": true,
+    "required_linear_history": false,
+    "allow_force_pushes": false,
+    "allow_deletions": false,
+    "required_conversation_resolution": true
+  
+})
