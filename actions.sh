6c4794b946e863fd306e52ac29774bbb2e19c634
Norbert Toth <118756875+dil-ntoth@users.noreply.github.com>
2023-07-12 14:34:38 +0200

    test 2

--- actions.sh
+++ actions.sh
@@ -1,7 +1,18 @@
+
+# Create
 curl -L \
   -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>"\
+  -H "Authorization: Bearer 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"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/OWNER/REPO/branches/BRANCH/protection \
-  -d '{"required_status_checks":{"strict":true,"contexts":["continuous-integration/travis-ci"]},"enforce_admins":true,"required_pull_request_reviews":{"dismissal_restrictions":{"users":["octocat"],"teams":["justice-league"]},"dismiss_stale_reviews":true,"require_code_owner_reviews":true,"required_approving_review_count":2,"require_last_push_approval":true,"bypass_pull_request_allowances":{"users":["octocat"],"teams":["justice-league"]}},"restrictions":{"users":["octocat"],"teams":["justice-league"],"apps":["super-ci"]},"required_linear_history":true,"allow_force_pushes":true,"allow_deletions":true,"block_creations":true,"required_conversation_resolution":true,"lock_branch":true,"allow_fork_syncing":true}'
+  https://api.github.com/repos/dil-ntoth/actions_test/branches/integration/protection \
+  -d '{"lock_branch":true, "enforce_admins":true,"required_pull_request_reviews":null,"required_status_checks":null,"restrictions":{users: [], teams: []}}'
+
+# Delete
+
+curl -L \
+  -X DELETE \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/dil-ntoth/actions_test/branches/integration/protection
