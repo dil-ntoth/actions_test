bd9a6975ec2ebf9475e0ca7ba434ab4aa087d33a
Norbert Toth <118756875+dil-ntoth@users.noreply.github.com>
2023-07-12 14:23:24 +0200

    test 2

--- actions.sh
+++ actions.sh
@@ -28,16 +28,20 @@ curl -L \
 
 
 
-
+# Create
 curl -L \
   -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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"\
+  -H "Authorization: Bearer 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"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/dil-ntoth/actions_test/branches/integration/protection \
   -d '{"lock_branch":true, "enforce_admins":true,"required_pull_request_reviews":null,"required_status_checks":null,"restrictions":{users: [], teams: []}}'
 
+# Delete
 
-
-
-
+curl -L \
+  -X DELETE \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/dil-ntoth/actions_test/branches/integration/protection
